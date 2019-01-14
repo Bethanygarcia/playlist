146635f45db9311590675db2ebb0dedcabcbce13
Bethany Garcia <bethanygarcia817@gmail.com>
2019-01-14 23:08:24 +0000

    Playlist

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["Hail to the King","Enter Sandman","Devil in I","The Fight Song"];
+var images = ["https://upload.wikimedia.org/wikipedia/en/thumb/4/46/A7XHailtotheKing.png/220px-A7XHailtotheKing.png", "http://www.custompresets.com/uploads/8/1/9/6/81965082/s908774473352204050_p189_i18_w480.jpeg", "https://upload.wikimedia.org/wikipedia/en/thumb/8/8b/The_Devil_In_I.jpg/220px-The_Devil_In_I.jpg", "https://i.ytimg.com/vi/KUgR7KdJUUk/maxresdefault.jpg"];
+var artists = ["Avenged Sevenfold","Metallica","Slipknot","Marilyn Manson"];
+var songLength = ["5:14","5:31","5:55","2:25"];
+var links = [];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
 
